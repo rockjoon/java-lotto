3487a1da34c6b5bdc04566da693c864ae0eaee0d
rockjoon <49752338+rockjoon@users.noreply.github.com>
2021-05-27 00:11:12 +0900

    test : 로또 당첨 번호와 일치하는 번호 개수 확인 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,12 +1,15 @@
 package lotto.domain;
 
-import lotto.domain.generator.AutoGeneratable;
+import lotto.domain.generator.AutoGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static lotto.domain.generator.LottoNumberGenerator.manualGenerate;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoTest {
@@ -14,10 +17,26 @@ class LottoTest {
     @DisplayName("램덤 번호로 로또 생성")
     @Test
     void lottoByAutoGenerator() {
-        AutoGeneratable autoGenerator = () -> Arrays.asList(1, 2, 3, 4, 5, 6);
+        AutoGenerator autoGenerator = () -> Arrays.asList(1, 2, 3, 4, 5, 6);
         List<Integer> numbers = autoGenerator.autoGenerate();
         Lotto lotto = new Lotto(numbers);
         assertThat(lotto.getLotto()).isEqualTo(numbers);
+    }
+
+    @DisplayName("당첨 번호와 일치하는 번호가 몇 개인지 확인")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1, 2, 3, 4, 5, 6 = 6",
+            "1, 2, 3, 4, 5, 45 = 5",
+            "1, 2, 3, 4, 44, 45 = 4",
+            "1, 2, 3, 43, 44, 45 = 3"
+    }, delimiter = '=')
+
+    void countSameNumber(String numbers, int expectedCount) {
+        Lotto winningLotto = new Lotto(manualGenerate("1, 2, 3, 4, 5, 6"));
+        Lotto lotto = new Lotto(manualGenerate(numbers));
+        int sameCount = lotto.countSameNumber(winningLotto);
+        assertThat(sameCount).isEqualTo(expectedCount);
     }
 
 }
