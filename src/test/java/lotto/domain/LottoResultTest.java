f4a03d6e734a66a8eff9d908022ddf0345d832b7
rockjoon <49752338+rockjoon@users.noreply.github.com>
2021-05-27 00:53:04 +0900

    test : 로또 당첨 종류에 따라 상금 구하기 테스트

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -4,24 +4,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static lotto.domain.LottoResult.*;
 
 class LottoResultTest {
 
-    @DisplayName("로또 티켓 수 구하기")
+    @DisplayName("당첨 종류에 따라 상금 구하기")
     @Test
-    void ticketCount() {
-        int purchasePrice = 14500;
-        int ticketCount = purchasePrice / LottoResult.PRICE_PER_TICKET;
-        LottoResult lottoResult = new LottoResult(purchasePrice);
-        assertThat(lottoResult.getTicketCount()).isEqualTo(ticketCount);
-    }
-
-    @DisplayName("구매 가능 금액 미달 시 IllegalArgumentException 발생")
-    @Test
-    void validatePurchasePrice() {
-        int purchasePrice = -1;
-        assertThatThrownBy(() -> new LottoResult(purchasePrice))
-                .isInstanceOf(IllegalArgumentException.class);
+    void findPrize() {
+        for (LottoResult result : values()) {
+            int prize = findPrizeByCount(result.sameCount);
+            assertThat(prize).isEqualTo(result.prize);
+        }
     }
 }
